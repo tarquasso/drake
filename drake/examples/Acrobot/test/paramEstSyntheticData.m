0e88850e9325374cd2cc6b61d74c36f9a4b073c5
Robert Katzschmann <rkk@csail.mit.edu>
2016-01-07 15:26:02 -0500

    playback swing up trajectory with slider before running parameter estimation

--- drake/examples/Acrobot/test/paramEstSyntheticData.m
+++ drake/examples/Acrobot/test/paramEstSyntheticData.m
@@ -1,4 +1,4 @@
-function paramEstSyntheticData
+%function paramEstSyntheticData
 
 oldpath = addpath(fullfile(pwd,'..'));
 
@@ -10,7 +10,9 @@ true_parameters = getParams(r);
 %% test on swingup up data
 [utraj,xtraj] = swingUpTrajectory(r);
 
-% Try parameter estimation without any noise
+v.playback(xtraj,struct('slider', true));
+
+%% Try parameter estimation without any noise
 Ts = .01; breaks=getBreaks(utraj); T0 = breaks(1); Tf = breaks(end);
 data = iddata(eval(xtraj,T0:Ts:Tf)',eval(utraj,T0:Ts:Tf)',Ts,'InputName',r.getInputFrame.getCoordinateNames(),'OutputName',r.getOutputFrame.getCoordinateNames());
 
