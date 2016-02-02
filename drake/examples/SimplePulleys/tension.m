1aacb81e403cfac690d0f933cde5c4e69f997879
Robert Katzschmann <rkk@csail.mit.edu>
2016-02-02 17:11:40 -0500

    - changes to initial parameter of tension.m - Comments to CableLength.m so that it is easier to read and uderstand/debug

--- drake/examples/SimplePulleys/tension.m
+++ drake/examples/SimplePulleys/tension.m
@@ -1,4 +1,4 @@
-function tension
+%function tension
 
 %r = PlanarRigidBodyManipulator('tension.urdf');
 r = TimeSteppingRigidBodyManipulator('tension.urdf',.01,struct('twoD',true));
@@ -8,7 +8,7 @@ v.xlim = [-5 5];
 v.ylim = [-.2 6.2];
 
 x0 = Point(getStateFrame(r));
-x0.load_x = 1;
+x0.load_x = 0;
 x0.load_z = 4;
 %x0.tensioner_angle = pi/2;
 %x0.load_zdot = -4;
