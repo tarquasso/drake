7fb1f5733c38dcaa7f2550bb06b1e4ab9575743d
Robert Katzschmann <rkk@csail.mit.edu>
2016-02-05 16:52:19 -0500

    further changes to tnesion parameter estiomation

--- drake/examples/SimplePulleys/tensionParamEstSyntheticDataURDF.m
+++ drake/examples/SimplePulleys/tensionParamEstSyntheticDataURDF.m
@@ -3,6 +3,9 @@ tStart0 = tic;
 tmp = addpathTemporary(fullfile(pwd,'..'));
 
 %% CONFIGURATION
+
+%Sampling Time
+Ts = 0.001;
 % Introduce parameter error into estimation
 hasParamErr = true;
 % Standard deviation of the parameter percent error
@@ -40,7 +43,7 @@ parameterEstimationOptions.print_result = 'noprint';
 % Forward simulation model to produce ground truth state trajectory
 % 'dircol'      = use direct collocation method (will not work with simerr)
 % 'euler'       = use forward Euler method
-simMethod = 'euler';
+simMethod = 'dircol';
 
 % Method by which to obtain qdd (only used in dynamic model)
 % 'manipul'     = Use acrobot manipulator equations to estimate true qdd
@@ -53,61 +56,69 @@ parameterEstimationOptions.C = eye(4);
 
 %% Initialize tension plants and variables
 % TODO: find out if just a PlanarRigidBodyManipulator would also work??
-rtrue =  TimeSteppingRigidBodyManipulator('tensionWParams.urdf',.01,struct('twoD',true));  
+rtrue =  TimeSteppingRigidBodyManipulator('tensionWParams.urdf',Ts,struct('twoD',true));  
 r = rtrue;
-nq = r.getNumPositions; %CHANGE TO BEFORE
+nq = r.getNumPositions;
 outputFrameNames = r.getOutputFrame.getCoordinateNames();
 p_orig = double(r.getParams);
 np = length(p_orig);
-pnames = getCoordinateNames(getParamFrame(r));
+%pnames = getCoordinateNames(getParamFrame(r));
+
+% Set Output Frame to what is observable
+% stateFrame = getStateFrame(r);
+% constructFrame
+% r.setOutputFrame(stateFrame);
 
 
 %% Initialize estimated tension system with parameter error
-%TODO: make consistent with parameter bounds
 if hasParamErr
   % Perturb original parameter estimates with random percentage error
   % normally distributed with standard dev = paramstd, and greater than -1
   rndVals = randn(1,np);
-  paramerr = rndVals*paramstd;
+  errorFactor = rndVals*paramstd;
   
-  % Not sure what the next three lines are good for?
-  %while sum(paramerr<=-1)~=0
-  %  paramerr(paramerr<-1) = randn(1,sum(paramerr<-1))*paramstd;
-  %end
+  %get parameter bounds of the parsed in tension plant
+  [pMin,pMax] = getParamLimits(r);
+  
   pErr = p_orig; %first copy true values to then disturb them
   for i = 1:np
-    pErr(i) = pErr(i)  + pErr(i) * paramerr(i); 
+    pErr(i) = pErr(i)  + pErr(i) * errorFactor(i);
+    
+    %Check if within parameter bounds
+    if(pErr(i) < pMin(i))
+      %set it to lower bound
+      pErr(i) = pMin(i);
+    elseif(pErr(i)> pMax(i))
+      %set it to upper bound
+      pErr(i) = pMax(i);
+    end
+    
   end
-  % rtrue.l1 = rtrue.l1 + rtrue.l1*paramerr(1);
-  % rtrue.l2 = rtrue.l2 + rtrue.l2*paramerr(2);
-  % rtrue.m1 = rtrue.m1 + rtrue.m1*paramerr(3);
-  % rtrue.m2 = rtrue.m2 + rtrue.m2*paramerr(4);
-%   pErr.b1  = pErr.b1 + pErr.b1*paramerr(5);
-%   pErr.b2  = pErr.b2 + pErr.b2*paramerr(6);
-%   pErr.lc1 = pErr.lc1 + pErr.lc1*paramerr(7);
-%   pErr.lc2 = pErr.lc2 + pErr.lc2*paramerr(8);
-%   pErr.Ic1 = pErr.Ic1 + pErr.Ic1*paramerr(9);
-%   pErr.Ic2 = pErr.Ic2 + pErr.Ic2*paramerr(10);
   r = r.setParams(pErr); %update the parameters and then copy it over (since no pass by reference)
 end
 
 %% Generate swingup data
-fprintf('Generating Disk Drops on Tensioner Trajectory...\n');
+fprintf('Generating Disk Drop on Tensioner Trajectory...\n');
 xtraj = diskOnTensionerDrop(rtrue); % Output is "Elapsed time is ... seconds"
 breaks=getBreaks(xtraj); T0 = breaks(1); Tf = breaks(end);
-Ts = 0.0001; %10 * (Tf-T0)/(numel(breaks)-1);
-tsamples = T0:Ts:Tf;
+%Ts = 10 * (Tf-T0)/(numel(breaks)-1);
+tsamples = breaks; %T0:Ts:Tf;
 if strcmp(simMethod,'dircol')
   fprintf('Computing Trajectory using Direct Collocation...\n');
   tStart1 = tic;
-  xsamples = eval(xtraj,tsamples)';
+  xsamplesFull = eval(xtraj,tsamples)';
   toc(tStart1)
 elseif strcmp(simMethod,'euler')
   fprintf('Computing Trajectory using Forward Euler Method...\n');
   tStart2 = tic;
-  xsamples = computeTraj(rtrue.getManipulator,eval(xtraj,T0),tsamples)'; %depending on step size, this might take some time to compute
+  xsamplesFull = computeTraj(rtrue.getManipulator,eval(xtraj,T0),tsamples)'; %depending on step size, this might take some time to compute
   toc(tStart2)
 else error('Must choose a simulation method'); end
+
+%% Remove Unobservable components
+% TODO shouldn't that be something that can be done in the simulate
+% function already?
+xsamples = xsamplesFull(:,[1:(nq-1),(nq+1):(2*nq-1)]); %theta_angle
 
 %% Add gaussian noise to measurements
 if hasMeasNoise
@@ -137,15 +148,15 @@ if strcmp(parameterEstimationOptions.model,'dynamic')
     qdd = qdd + randn(size(qdd))*diag(noisestd((end+1-size(qdd,2)):end));
   end
   xsamplesfinal = [xsamplesnoisy(1:length(qdd),:), qdd];
-  usamples = usamples(1:length(qdd),:);
-  outputFrameNames = [outputFrameNames;'theta1doubledot';'theta2doubledot'];
+  %usamples = usamples(1:length(qdd),:);
+  outputFrameNames = [outputFrameNames;'load_xdotdot';'load_zdotdot';'tensioner_angledotdot'];
 else
   xsamplesfinal = xsamplesnoisy;
 end
 
 fprintf('Perform Parameter Estimation ...\n');
 tStart3 = tic;
-data = iddata(xsamplesfinal,usamples,Ts,'InputName',r.getInputFrame.getCoordinateNames(),'OutputName',outputFrameNames);
+data = iddata(xsamplesfinal,[],Ts,'OutputName',outputFrameNames); %'InputName',r.getInputFrame.getCoordinateNames(),
 [estimated_parameters,simerror] = parameterEstimation(r,data,parameterEstimationOptions);
 toc(tStart3)
 
@@ -171,7 +182,7 @@ dt = diff(t);
 N = length(dt);
 xtraj = [x0,zeros(nx,N)];
 for i=1:N
-  f = dynamics(obj,t(i),xtraj(:,i));
+  f = dynamics(obj,t(i),xtraj(:,i)); %TODO Speed this up!
   xtraj(:,i+1) = xtraj(:,i)+f*dt(i);
 end
 end
@@ -184,7 +195,7 @@ x0.load_z = 3.99;
 x0.load_zdot = -2.5; %starting velocity
 x0 = resolveConstraints(obj,x0);
 
-xtraj = simulate(obj,[0 4],x0);
+xtraj = simulate(obj,[0 1],x0);
 
 end
 
