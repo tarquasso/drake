dd968efd1ab6008ec17edd666bf2d65e5203df20
Robert Katzschmann <rkk@csail.mit.edu>
2016-11-08 17:34:02 -0500

    add comment to dummy function for test parameter estimation

--- drake/examples/SimplePulleys/paramest/testparamEst.m
+++ drake/examples/SimplePulleys/paramest/testparamEst.m
@@ -2,7 +2,7 @@ q = rand(10,3);
 qd = 10*rand(10,3);
 qdd = 100*rand(100,3);
 
-p0 = rand(4,1);
+p0 = rand(4,1); % simulation hand tuning needed for this parameter
 
 fun = @(p) paramEstCostFun(p, q, qd, qdd);
 options = optimoptions(@fmincon);
