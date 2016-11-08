63208fc8c5ff15e624f1668ee0416498c0c92d89
Robert Katzschmann <rkk@csail.mit.edu>
2016-11-08 09:34:05 -0500

    update tension model with parameters

--- drake/examples/SimplePulleys/tensionWParams.m
+++ drake/examples/SimplePulleys/tensionWParams.m
@@ -6,14 +6,14 @@ r = PlanarRigidBodyManipulator('tensionWParams.urdf');
 trueParameters = getParams(r);
 
 v = r.constructVisualizer();
-v.xlim = [-3.31 -2.25];
-v.ylim = [2.5 3.5];
+v.xlim = [-3.1 -2.512];
+v.ylim = [2.79 3.21];
 
 x0 = Point(getStateFrame(r));
-x0.load_x = -2.811;
-x0.load_z = 3.0885;
+x0.load_x = -2.811; %TODO: adjust x point of collision, we canassume it is center
+x0.load_z = 3.0+88.46/2/1000; % radius of sphre is  
 x0.tensioner_angle = .1414;%pi/2;
-%x0.load_zdot = -4;
+x0.load_zdot = -2; %TODO: take original speed from capture data
 
 
 
@@ -29,7 +29,7 @@ v.drawWrapper(0,x0(1:3));
 x0 = resolveConstraints(r,x0,v);
 v.drawWrapper(0,x0(1:3));
 
-ytraj = simulate(r,[0 8],x0);
+ytraj = simulate(r,[0 0.22],x0);
 len = [];
 if(1)
 ts = ytraj.getBreaks();
