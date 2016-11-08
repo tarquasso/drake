b2d29fc97d68b50da5abb5ba31d069e5a38a3ced
Robert Katzschmann <rkk@csail.mit.edu>
2016-11-08 17:53:42 -0500

    reformatting paramestcostfun slightly

--- drake/examples/SimplePulleys/paramest/paramEstCostFun.m
+++ drake/examples/SimplePulleys/paramest/paramEstCostFun.m
@@ -2,6 +2,9 @@ function f = paramEstCostFun(p, q, qd, qdd)
 
 r = PlanarRigidBodyManipulator('tensionWParamsExp.urdf');
 
+% columns are the states
+% rows are samples
+
 theta = q(:,1);
 thetad = qd(:,1);
 thetadd = qdd(:,1);
@@ -9,19 +12,31 @@ xdd = qdd(:,2);
 zdd = qdd(:,3);
 
 g = 9.81;
-beta = 30*pi/180;
+beta = 30*pi/180; % angle of the sliding platform for the disk
 
 Ipulley = p(1);
 mball = p(2);
-k = p(3);
-b = p(4);
+kpulley = p(3);
+bpulley = p(4);
 
 psi = NaN(length(theta),1);
 for i = 1:length(theta)
     H = diag([Ipulley, mball, mball]);
-    C = [b*thetad(i) + k*theta(i); 0; mball*g*cos(beta)];
+    C = [ bpulley*thetad(i) + kpulley*theta(i); ...
+          0;...
+          mball*g*cos(beta)];
     
-    % [H,C,~] = manipulatorDynamics(r, q, qd);
+    % q=msspoly('q',nq);
+    % s=msspoly('s',nq);
+    % c=msspoly('c',nq);
+    % qt=TrigPoly(q,s,c);
+    % qd=msspoly('qd',nq);
+    % qdd=msspoly('qdd',nq);
+    % u=msspoly('u',nu);
+    % p=r1.getParamFrame.getPoly;
+    % pr1 = setParams(r1,p);
+    % [H,C,B] = manipulatorDynamics(r,qt,qd);
+    % % we do not need B here
     
     [~, J, d2phidq2] = r.position_constraints{1}.eval(q(i,:)');
     Jdot = (reshape(d2phidq2,length(q(i,:)'),[])*qd(i,:)')';
