f10f1663ee86148263a896df9fe550f81cc2d4b4
Robert Katzschmann <rkk@csail.mit.edu>
2016-11-10 09:38:07 -0500

    add fitting for theta - not working yet

--- drake/examples/SimplePulleys/paramest/tensionParamEst.m
+++ drake/examples/SimplePulleys/paramest/tensionParamEst.m
@@ -14,17 +14,17 @@ zVals = History.objectPosition(1:nos,1); % x points up on the plane
 xVals = History.objectPosition(1:nos,2);
 yVals = History.objectPosition(1:nos,3);
 
-figure(10); clf; plot(timeVals,zVals,'b'); hold on; xlabel('time [s]'); 
+figure(10); clf; plot(timeVals,zVals,'b'); hold on; xlabel('time [s]');
 ylabel('height coordinate z [m]'); title('Check Z Coordinate of Data Set for limit values!')
 
-figure(11); clf; plot(timeVals,xVals,'b'); hold on; xlabel('time [s]'); 
-ylabel('horizontal coordinate x [m]'); title('Check X Coordinate of Data Set for Skewedness of Data!')  
+figure(11); clf; plot(timeVals,xVals,'b'); hold on; xlabel('time [s]');
+ylabel('horizontal coordinate x [m]'); title('Check X Coordinate of Data Set for Skewedness of Data!')
 
 %% Fill in observed data of that data setS
 % Set 5 Touching point: 55.8mm - manually measured from the Optitrack
 
 %Static spring stretching point: 52.8 mm
-expStartTime = 5.35; %seconds 5.4 
+expStartTime = 5.35; %seconds 5.4
 expEndTime = 8; %seconds 8.2
 
 % I used a 250mm wand, but told the mpotive software that it is a 500mm
@@ -42,18 +42,22 @@ discRadius = 0.044230;
 
 % filename = '~/soft_modeling_repo/dev/tracking/data/set6/16-May-2015 20_53_54.mat';
 % tp = 0.0558;
-% expStartTime = 0; %seconds 5.4 
+% expStartTime = 0; %seconds 5.4
 % expEndTime = 6.7; %seconds 8.2
 
 
-generatePlot = true; 
-% [times, z,zd,zdd, tICE, tICES, xICE, zICE, numOfSets, qfit, gof] = ...
-%   parseTensionExperimentData(filename,tp,expStartTime,expEndTime,optiTrackWandErrorFactor,minHeight,maxHeight,generatePlot);
+generatePlot = true;
+parseExpFlag = true;
 
-%save('data.mat', 'times', 'z','zd','zdd')
-
-load('data.mat')
-
+if(parseExpFlag)
+  
+  [times, z,zd,zdd, tICE, tICES, xICE, zICE, numOfSets, qfit, gofTheta] = ...
+    parseTensionExperimentData(filename,tp,expStartTime,expEndTime,optiTrackWandErrorFactor,minHeight,maxHeight,generatePlot);
+  
+  save('data.mat', 'times', 'z','zd','zdd');
+else
+  load('data.mat');
+end
 
 %j=1;
 %m = length(z{j});
@@ -61,50 +65,93 @@ load('data.mat')
 %q = [zeros(m,2);z{1}];
 theta0 = 0;
 thetad0 = -0;
+numOfSets = size(z,1);
+for j = 1:numOfSets
+z{j} = z{j} + discRadius; % shift the z coordinate by the disc radius up
+end
 
-z{1} = z{1} + discRadius; % shift the z coordinate by the disc radius up
+
 
 options = optimoptions(@fmincon);
 options = optimoptions(options, 'SpecifyObjectiveGradient', true, 'Display', 'none');
- lb = [-pi;-100];
- ub = [pi;100];
- numSets = size(z,1);
-%thetaVec = cell(numSets,1);
-  thetaVec = zeros(size(z{1},1),2);
+lb = [-pi;-100];
+ub = [pi;100];
+
+theta = cell(numOfSets,1);
+thetadfmincon = cell(numOfSets,1);
+%thetaVec = zeros(size(z{1},1),2);
 %theta = zeros(size(z{1},1),1);
-for k =1:size(z{1},1)
-fun = @(thetaVec) resolveConstraintThetaThetaDotCostFun([thetaVec(1);0;z{1}(k)], [thetaVec(2);0;zd{1}(k)]);
-[thetaVec(k,:),fval] = fmincon(fun, [theta0; thetad0], [], [], [], [], lb, ub, [], options);
 
-% fun = @(theta) resolveConstraintThetaCostFun([theta;0;z{1}(k)]);
-%  [theta(k),fval] = fmincon(fun, theta0, [], [], [], [], lb(1), ub(1), [], options);
+calcThetaFlag = true;
 
-theta0 = thetaVec(k,1);
-thetad0 = thetaVec(k,2);
+if(calcThetaFlag)
+  for j = 1:numOfSets
+    for k =1:size(z{j},1)
+      fun = @(thetaVec) resolveConstraintThetaThetaDotCostFun([thetaVec(1);0;z{j}(k)], [thetaVec(2);0;zd{j}(k)]);
+      [thetaVecTemp,fval] = fmincon(fun, [theta0; thetad0], [], [], [], [], lb, ub, [], options);
+      theta{j}(k) = thetaVecTemp(1);
+      thetadfmincon{j}(k) = thetaVecTemp(2);
+      % fun = @(theta) resolveConstraintThetaCostFun([theta;0;z{j}(k)]);
+      %  [theta(k),fval] = fmincon(fun, theta0, [], [], [], [], lb(1), ub(1), [], options);
+      
+      theta0 = thetaVecTemp(1);
+      thetad0 = thetaVecTemp(2);
+  
+    end
+  end
+  save('data2.mat', 'times', 'z','zd','zdd','theta','numOfSets','thetadfmincon');
+else
+  load('data2.mat');
 end
 
-%% Initial Parameter Guesses
-min = 0.00000001;
-max = 100;
+%% fit curves for theta
+thetafit = cell( numOfSets, 1 );
+thetad = cell( numOfSets, 1 );
+thetadd = cell( numOfSets, 1 );
 
-Ipulley = 0.00001798;
-kpulley = 0.22;
-bpulley = 0.0024;
-% note: mdisc is not a parameter to be estimated
+gofTheta = struct( 'sse', cell( numOfSets, 1 ), ...
+  'rsquare', [], 'dfe', [], 'adjrsquare', [], 'rmse', [] );
+%ft = fittype( 'poly2' );
+ft = fittype( 'cubicinterp' );
 
-p0 = [Ipulley;kpulley;bpulley]; % simulation hand tuning
-dimParams = length(p0);
+times = cell( numOfSets, 1 );
+for j = 1: numOfSets
 
+  figure(501); clf; hold on; plot(times{j}, z{j},'b*');
+  [tData, thetaData] = prepareCurveData(  times{j}, theta{j});
+  
+  figure(501); clf; hold on; plot(tData, thetaData,'r+');
+  [thetafit{j}, gofTheta(j)] = fit( tData, thetaData, ft );
+  thetaCmp{j} = feval(thetafit{j},times{j});
+  [thetad{j}, thetadd{j}] = differentiate(thetafit{j},times{j});
 
-fun = @(p) paramEstCostFun(p, q, qd, qdd, angleDeg, mdisc);
-options = optimoptions(@fmincon);
-options = optimoptions(options, 'Display', 'iter');
+  figure(503)
+  p = plot(thetafit{j},tData,thetaData);%,[timeInterval{j}])      
+  p(1).LineWidth = 2;
+end
 
-[pEstimated,fval] = fmincon(fun, p0, [],[], [], [], ...
-           min*ones(dimParams,1), max*ones(dimParams,1),[], options);
-         
-IpulleyEst = pEstimated(1);
-kpulleyEst = pEstimated(2);
-bpulleyEst = pEstimated(3);
-
-IpulleyEst,kpulleyEst,bpulleyEst
+% %% Initial Parameter Guesses
+% min = 0.00000001;
+% max = 100;
+% 
+% Ipulley = 0.00001798;
+% kpulley = 0.22;
+% bpulley = 0.0024;
+% % note: mdisc is not a parameter to be estimated
+% 
+% p0 = [Ipulley;kpulley;bpulley]; % simulation hand tuning
+% dimParams = length(p0);
+% 
+% 
+% fun = @(p) paramEstCostFun(p, q, qd, qdd, angleDeg, mdisc);
+% options = optimoptions(@fmincon);
+% options = optimoptions(options, 'Display', 'iter');
+% 
+% [pEstimated,fval] = fmincon(fun, p0, [],[], [], [], ...
+%   min*ones(dimParams,1), max*ones(dimParams,1),[], options);
+% 
+% IpulleyEst = pEstimated(1);
+% kpulleyEst = pEstimated(2);
+% bpulleyEst = pEstimated(3);
+% 
+% IpulleyEst,kpulleyEst,bpulleyEst
