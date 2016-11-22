052207a2b2102f32dc6d4afce90626b1fcae057a
Robert Katzschmann <rkk@csail.mit.edu>
2016-11-22 15:18:44 -0500

    add sliding window fitting and data splitting

--- drake/examples/SimplePulleys/paramest/dataPreProcessing/processCaptureData.m
+++ drake/examples/SimplePulleys/paramest/dataPreProcessing/processCaptureData.m
@@ -6,7 +6,7 @@ function [] = processCaptureData(filename,expStartTime,expEndTime,maxHeightAfter
 %%%%%%%%%%%%%%%%%%
 global generatePlot
 generatePlot = true;
-
+splitFit = true;
 
 filenameWithExtension = [filename,'.mat'];
 load(filenameWithExtension);
@@ -26,21 +26,21 @@ zTouch = discRadius; %touchpoint is measured based on the center of the disc
 
 %% Plot the data Before Defining
 if(generatePlot)
-yLabels = { 'height coordinate z [m]',...
-  'horizontal coordinate x [m]',...
-  'normal to plane coordinate y[m]'};
-titleLabels = {'Unformatted Data - Check Z Coordinate of Data Set for limit values!',...
-  'Unformatted Data - Check X Coordinate of Data Set for Skewedness of Data!',...
-  'Unformatted Data - Check Y Coordinate of Data Set for Lifting off table!'};
-for i = 1:1 %only plot z axis
-  figure(i); clf; plot(timeStepsOrig,posDiscScaled(:,i),'b'); hold on; xlabel('time [s]');
-  if(i == 1) %for z axis
-    plot(timeStepsOrig([1,end]),[touchPointAfterScaling,touchPointAfterScaling],'g','LineWidth',2)
-    plot(timeStepsOrig([1,end]),[staticSpringStrectchingPointAfterScaling,staticSpringStrectchingPointAfterScaling],'y','LineWidth',2)
-    
+  yLabels = { 'height coordinate z [m]',...
+    'horizontal coordinate x [m]',...
+    'normal to plane coordinate y[m]'};
+  titleLabels = {'Unformatted Data - Check Z Coordinate of Data Set for limit values!',...
+    'Unformatted Data - Check X Coordinate of Data Set for Skewedness of Data!',...
+    'Unformatted Data - Check Y Coordinate of Data Set for Lifting off table!'};
+  for i = 1:1 %only plot z axis
+    figure(i); clf; plot(timeStepsOrig,posDiscScaled(:,i),'b'); hold on; xlabel('time [s]');
+    if(i == 1) %for z axis
+      plot(timeStepsOrig([1,end]),[touchPointAfterScaling,touchPointAfterScaling],'g','LineWidth',2)
+      plot(timeStepsOrig([1,end]),[staticSpringStrectchingPointAfterScaling,staticSpringStrectchingPointAfterScaling],'y','LineWidth',2)
+      
+    end
+    ylabel(yLabels{i}); title(titleLabels{i})
   end
-  ylabel(yLabels{i}); title(titleLabels{i})
-end
 end
 
 %Adjust Time Range and remove one error
@@ -63,23 +63,23 @@ posDisc(:,3) = posDisc(:,3) - touchPointAfterScaling + discRadius;
 
 %% Plot the data After Defining
 if(generatePlot)
-yLabels2 = { 'horizontal coordinate x [m]',...
-  'normal to plane coordinate y[m]'...
-  'height coordinate z [m]'};
-
-titleLabels2 = {'X Coordinate of Data Set',...
-  'Y Coordinate of Data Set',...
-  'Z Coordinate of Data Set'};
-for i = 1:3
-  figure(i+10); clf; plot(timeSteps,posDisc(:,i),'b','LineWidth',0.6); hold on; xlabel('time [s]');
-  ylabel(yLabels2{i}); title(titleLabels2{i})
-  if(i == 3) %for z axis
-    plot(timeSteps([1,end]),[zTouch,zTouch],'g','LineWidth',2)    
+  yLabels2 = { 'horizontal coordinate x [m]',...
+    'normal to plane coordinate y[m]'...
+    'height coordinate z [m]'};
+  
+  titleLabels2 = {'X Coordinate of Data Set',...
+    'Y Coordinate of Data Set',...
+    'Z Coordinate of Data Set'};
+  for i = 1:3
+    figure(i+10); clf; plot(timeSteps,posDisc(:,i),'b','LineWidth',0.6); hold on; xlabel('time [s]');
+    ylabel(yLabels2{i}); title(titleLabels2{i})
+    if(i == 3) %for z axis
+      plot(timeSteps([1,end]),[zTouch,zTouch],'g','LineWidth',2)
+    end
+    title([titleLabels2{i},' (',dataSetName,')'])
+    options.Format = 'eps';
+    hgexport(gcf,[pathstr,'/plots/',name,'_',titleLabels2{i},'.eps'],options);
   end
-  title([titleLabels2{i},' (',dataSetName,')'])
-  options.Format = 'eps';
-  hgexport(gcf,[pathstr,'/plots/',name,'_',titleLabels2{i},'.eps'],options);
-end
   
 end
 
@@ -87,6 +87,7 @@ end
 
 idxNC = find(posDisc(:,3) > zTouch);
 idxNC = idxNC(2:end-1);
+  
 idxIC = find(posDisc(:,3) <= zTouch);
 idxIC = idxIC(1:end-1);
 
@@ -95,18 +96,18 @@ timeStepsIC = timeSteps(idxIC);
 posDiscNC = posDisc(idxNC,:);
 posDiscIC = posDisc(idxIC,:);
 
-minHeight = minHeightAfterScaling - touchPointAfterScaling + discRadius;
-maxHeight = maxHeightAfterScaling - touchPointAfterScaling + discRadius;
+%minHeight = minHeightAfterScaling - touchPointAfterScaling + discRadius;
+%maxHeight = maxHeightAfterScaling - touchPointAfterScaling + discRadius;
 
 if(generatePlot)
-  %Add a line to the z coordinate 
+  %Add a line to the z coordinate
   figure(21); clf; hold on
   plot(timeSteps,posDisc(:,i),'g','LineWidth',1.0);
   plot(timeStepsNC,posDiscNC(:,3),'b.','LineWidth',2.0)
   plot(timeSteps([1,end]),[zTouch,zTouch],'g','LineWidth',2)
   plot(timeStepsIC,posDiscIC(:,3),'r.','LineWidth',2.0)
   xlabel('time [s]')
-  %axis([-inf inf minHeight maxHeight])  
+  %axis([-inf inf minHeight maxHeight])
   title(['Height z - Unseparated (',dataSetName,')'])
   typeofPlot = 'z';
   hgexport(gcf,[pathstr,'/plots/',name,'_',typeofPlot,'.eps'],options);
@@ -127,7 +128,7 @@ if(generatePlot)
   xlabel('time [s]')
   ylabel('zd')
   title(['Velocity zd',' (',dataSetName,')'])
-
+  
   figure(24);clf; hold on;
   xlabel('time [s]')
   ylabel('zdd')
@@ -135,31 +136,41 @@ if(generatePlot)
   
 end
 
+typeNC = 'NoSplit';
 offsetStepNC = 0; %defines additional points looked at before or after a contact data set
-minDataPointsNC = 4;
-[numOfSetsNC,timeStepsSplitNC,posDiscSplitNC] = extractSets(idxNC,timeSteps,posDisc,offsetStepNC,minDataPointsNC);
-offsetStepIC = 1;
+minDataPointsNC = 8;
+[numOfSetsNC,timeStepsSplitNC,posDiscSplitNC] = extractSets(idxNC,timeSteps,posDisc,offsetStepNC,minDataPointsNC,typeNC);
+
+typeIC = 'NoSplit';
+offsetStepIC = 1; % taking one additional data point at start and end of the data set
 minDataPointsIC = 5;
-[numOfSetsIC,timeStepsSplitIC,posDiscSplitIC] = extractSets(idxIC,timeSteps,posDisc,offsetStepIC,minDataPointsIC);
+[numOfSetsIC,timeStepsSplitIC,posDiscSplitIC] = extractSets(idxIC,timeSteps,posDisc,offsetStepIC,minDataPointsIC,typeIC);
 
-
+if(splitFit)
+%% Setting up the fitting for the non contact phase
+[timeStepsNC,zNC,zdNC,zddNC] ...
+  = fitCurvesSlidingWindow(numOfSetsNC,timeStepsSplitNC, posDiscSplitNC);
+%% Setting up the fitting for the contact phase
+[timeStepsIC,zIC,zdIC,zddIC] ...
+  = fitCurvesSlidingWindow(numOfSetsIC,timeStepsSplitIC, posDiscSplitIC);
+else
+  %% Setting up the fitting for the non contact phase
 ftNC = fittype( 'poly4' ); %results in a linear model for the acceleration change due to sliding friction
 zdd_lbNC = -inf;
 zdd_ubNC = 0.0;
 [timeStepsNC,zNC,zdNC,zddNC,timeStepsLargeNC,zLargeNC,zdLargeNC,zddLargeNC,zfitNC] ...
   = fitCurves(numOfSetsNC,timeStepsSplitNC, posDiscSplitNC,offsetStepNC,ftNC,zdd_lbNC,zdd_ubNC);
 
-
-
 %% Setting up the fitting for the contact phase
 ftIC = fittype( 'poly4' );
 zdd_lbIC = -sind(angleDeg)*9.81*1/2; %half the gravitational force because we do not know the sliding friction yet
+%zdd_lbIC = -inf; %half the gravitational force because we do not know the sliding friction yet
 zdd_ubIC = inf;
 [timeStepsIC,zIC,zdIC,zddIC,timeStepsLargeIC,zLargeIC,zdLargeIC,zddLargeIC,zfitIC] ...
   = fitCurves(numOfSetsIC,timeStepsSplitIC, posDiscSplitIC,offsetStepIC,ftIC,zdd_lbIC,zdd_ubIC);
+end
 
 %% more plotting
-
 if(generatePlot)
   figure(22)
   typeofPlot = 'z';
@@ -171,7 +182,7 @@ if(generatePlot)
   typeofPlot = 'zdd';
   hgexport(gcf,[pathstr,'/plots/',name,'_',typeofPlot,'.eps'],options);
 end
-  
+
 
 % resave the data set
 newFilename = [filename,'_preprocessed.mat'];
@@ -182,7 +193,7 @@ save(newFilename,'angleDeg','mdisc','spread','zTouch',...
 
 end
 
-function [numOfSets,timeStepsSplit,posDiscSplit] = extractSets(idxMode,timeSteps, posDisc,offsetStep,minDataPoints)
+function [numOfSetsNew,timeStepsSplit,posDiscSplit] = extractSets(idxMode,timeSteps, posDisc,offsetStep,minDataPoints,type)
 global generatePlot
 %% Define points when in contact and points when not in contact
 boolTransitions = diff(idxMode)> 1;
@@ -191,24 +202,52 @@ boolTransitionsStarts = [true;boolTransitions];
 idxStart =  idxMode(boolTransitionsStarts);
 boolTransitionsEnds = [boolTransitions;true];
 idxEnd = idxMode(boolTransitionsEnds);
-%Remove last set if contains to little data:
 
+%Remove last set if contains to little data:
 if (idxEnd(end)-idxStart(end)<=minDataPoints)
-idxStart = idxStart(1:end-1);
-idxEnd = idxEnd(1:end-1);
+  idxStart = idxStart(1:end-1);
+  idxEnd = idxEnd(1:end-1);
 end
+
 numOfSets = length(idxEnd);%sum(boolTransitionsStarts);
 
-idxExtended = cell(numOfSets,1);
-timeStepsSplit = cell(numOfSets,1);
-posDiscSplit = cell(numOfSets,1);
+if(strcmp(type,'Split')) % no contact
+  
+idxStartNew = [];
+idxEndNew = [];
+for l = 1:numOfSets
+  idxCheck = (idxStart(l)):(idxEnd(l));
+  zDataRelevant = posDisc(idxCheck,3);
+  
+  [~,I] = max(zDataRelevant);
+  
+  if(I==1)
+    idxEndNew = [idxEndNew,idxEnd(l)];
+    idxStartNew = [idxStartNew,idxStart(l)];
+  else
+    idxEndNew = [idxEndNew,idxCheck(I)-1,idxEnd(l)];
+    idxStartNew = [idxStartNew,idxStart(l),idxCheck(I)+1];
+    
+  end
+end
+
+else
+    idxStartNew = idxStart;
+    idxEndNew = idxEnd;
+end
+  
+numOfSetsNew = length(idxEndNew);%sum(boolTransitionsStarts);
+
+idxExtended = cell(numOfSetsNew,1);
+timeStepsSplit = cell(numOfSetsNew,1);
+posDiscSplit = cell(numOfSetsNew,1);
 
 
-for j = 1:numOfSets
-  idxExtended{j} = (idxStart(j)-offsetStep):(idxEnd(j)+offsetStep);
+for j = 1:numOfSetsNew
+  idxExtended{j} = (idxStartNew(j)-offsetStep):(idxEndNew(j)+offsetStep);
   timeStepsSplit{j} = timeSteps(idxExtended{j});
   posDiscSplit{j} = posDisc(idxExtended{j},:);
-  %   tICES{j} = tICE{j}-tICE{j}(1);
+   
   if(generatePlot)
     figure(22);
     plot(timeStepsSplit{j}(1),posDiscSplit{j}(1,3),'m*','LineWidth',4.5)
@@ -228,6 +267,7 @@ zfit = cell( numOfSets, 1 );
 timeStepsExpanded = cell( numOfSets, 1 );
 timeStepsExpandedUseful= cell( numOfSets, 1 );
 z = cell( numOfSets, 1 );
+z_pred = cell( numOfSets, 1 );
 zd = cell( numOfSets, 1 );
 zdd = cell( numOfSets, 1 );
 timeStepsLarge = cell( numOfSets, 1 );
@@ -237,10 +277,6 @@ zddLarge = cell( numOfSets, 1 );
 
 gof = struct( 'sse', cell( numOfSets, 1 ), ...
   'rsquare', [], 'dfe', [], 'adjrsquare', [], 'rmse', [] );
-%ft = fittype( 'poly2' );
-%ft = fittype( 'cubicinterp' );
-
-
 
 for j = 1: numOfSets
   %figure(21+10*j); clf; hold on; plot(timeSteps{j}, posDisc{j}(:,3),'*');
@@ -283,27 +319,25 @@ for j = 1: numOfSets
   %timeStepsExpanded{j}  = linspace(timeStepsExpanded{j}(1),timeStepsExpanded{j}(end),elementsToCheck);
   
   z{j} = feval(zfit{j},timeStepsExpanded{j});
-  z{j} = z{j};% + zMean;
-  
+  z_pred{j} = predint(zfit{j},timeStepsExpanded{j},0.95,'observation','off');
+
   [zd{j}, zdd{j}] = differentiate(zfit{j},timeStepsExpanded{j});
-  
-  % scale the
-  timeStepsExpanded{j} = timeStepsExpanded{j};% + tMean;
-  
-  
+    
   idxUseful = find(zdd{j} > zdd_lb & zdd{j} < zdd_ub);
-  
+    
   timeStepsExpandedUseful{j} = timeStepsExpanded{j}(idxUseful);
   z{j} = z{j}(idxUseful);
   zd{j} = zd{j}(idxUseful);
   zdd{j} = zdd{j}(idxUseful);
   
-  if(generatePlot)  
+  if(generatePlot)
     figure(22);
     p1 = plot(timeStepsExpandedUseful{j},z{j});
     %     p1 = plot(zfit{j},timeStepsExpanded{j},z{j});%,[timeInterval{j}])\
     p1(1).LineWidth = 1;
-    
+    plot(timeStepsExpanded{j},z_pred{j},'r');
+    %plot(zfit{j},'predobs');
+        
     figure(23)
     p2 = plot(timeStepsExpandedUseful{j},zd{j},'.');
     p2(1).LineWidth = 1;
@@ -330,6 +364,56 @@ for j = 1: numOfSets
   zddLarge{j} = zddLarge{j}(idxUseful);
   
 end
+end
 
+function [timeSteps,z,zd,zdd] = ...
+  fitCurvesSlidingWindow(numOfSets,timeSteps, posDisc)
 
+global generatePlot
+
+%% fit curves
+zfit = cell( numOfSets, 1 );
+z = cell( numOfSets, 1 );
+zpred = cell( numOfSets, 1 );
+zd = cell( numOfSets, 1 );
+zdd = cell( numOfSets, 1 );
+gof = struct( 'sse', cell( numOfSets, 1 ), ...
+  'rsquare', [], 'dfe', [], 'adjrsquare', [], 'rmse', [] );
+ft = fittype( 'poly2' );
+
+for j = 1: numOfSets
+  numOfSteps = length( timeSteps{j});
+  for k = 1:(numOfSteps)
+    indices = max(1,k-3):1:min(numOfSteps,k+3);
+    timeStepsWindow =  timeSteps{j}(indices);
+    zDataWindow = posDisc{j}(indices,3);
+    [tData, zData] = prepareCurveData(timeStepsWindow, zDataWindow );
+    
+    [zfit{j}, gof(j)] = fit( tData, zData, ft ,'Normalize','on');
+    z{j}(k) = feval(zfit{j},timeSteps{j}(k));
+    zpredLocal = predint(zfit{j},timeSteps{j}(k), 0.95,'functional','off');
+    zpred{j}(k,:) = zpredLocal;
+    
+    [zd{j}(k), zdd{j}(k)] = differentiate(zfit{j},timeSteps{j}(k));
+  end
+  
+  if(generatePlot)
+    figure(22);
+    p1 = plot(timeSteps{j},z{j});
+    p1(1).LineWidth = 1;
+    %plot the predicition bounds:
+    plot(timeSteps{j},zpred{j},'r');
+
+    figure(23)
+    p2 = plot(timeSteps{j},zd{j},'.');
+    p2(1).LineWidth = 1;
+    plot(timeSteps{j},zd{j});
+    
+    figure(24)
+    p3 = plot(timeSteps{j},zdd{j},'.');
+    p3(1).LineWidth = 1;
+    plot(timeSteps{j},zdd{j});
+  end
+  
+end
 end
