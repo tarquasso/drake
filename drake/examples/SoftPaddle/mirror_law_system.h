19e2dc50e95a18f65e38fb468b7b6f91abf0cc19
Robert Katzschmann <rkk@csail.mit.edu>
2017-03-23 13:48:43 -0400

    ~

--- drake/examples/SoftPaddle/mirror_law_system.h
+++ drake/examples/SoftPaddle/mirror_law_system.h
@@ -9,7 +9,7 @@
 #include "drake/systems/framework/context.h"
 #include "drake/systems/framework/diagram.h"
 #include "drake/systems/framework/leaf_system.h"
-#include "drake/systems/framework/system_output.h"
+#include "drake/systems/framework/output_port_value.h"
 
 namespace drake {
 namespace examples {
